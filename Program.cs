fb1d309f6584f6c0accf1fcedacefc8b59f19b86
dk.raw <hello@dkatsios.tk>
2021-05-15 13:47:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountVowels("dogecoin");
+        }
+        private static void CountVowels(string str)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'o' || str[i] == 'u' || str[i] == 'i' || str[i] == 'y' || str[i] == 'e')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
